6e9c86b9375b0bd12b0035b0a11b6ed6b3296355
mohammedhrima <mohammed.hrima1998@gmail.com>
2024-11-07 20:13:26 +0100

    update

--- project/out/pages/Home/Home.js
+++ project/out/pages/Home/Home.js
@@ -1,43 +1,4 @@
 import Ura from "ura";
-class Comp {
-    constructor() {
-        this.index = 1; // Initialize index here to avoid undefined error
-        this.vdom = null;
-        this.states = {};
-        this.view = () => Ura.element("empty", null);
-    }
-    State(initialValue) {
-        const stateIndex = this.index++;
-        this.states[stateIndex] = initialValue;
-        const getter = () => this.states[stateIndex];
-        const setter = (newValue) => {
-            if (!Ura.deepEqual(this.states[stateIndex], newValue)) {
-                this.states[stateIndex] = newValue;
-                this.updateState();
-            }
-        };
-        return [getter, setter];
-    }
-    updateState() {
-        console.log("call updateState");
-        const newVDOM = this.view();
-        if (this.vdom) {
-            console.log("old:", this.vdom);
-            console.log("new:", newVDOM);
-            Ura.reconciliate(this.vdom, newVDOM);
-        }
-        else {
-            this.vdom = newVDOM;
-        }
-    }
-    render(call) {
-        console.log("render :", call);
-        this.view = call;
-        this.vdom = call();
-        return this.vdom;
-    }
-}
-// Component definitions
 function Child(props) {
     const [render, State] = Ura.init();
     const [getter, setter] = State(20);
@@ -56,7 +17,6 @@ function Tag() {
         Ura.element("h1", null,
             "hello world ",
             getter()),
-        Ura.element("button", { onclick: () => setter(getter() + 1) }, "click"),
-        Ura.element(Child, { id: getter() }))));
+        Ura.element("button", { onclick: () => setter(getter() + 1) }, "click"))));
 }
 export default Tag;
--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -1,50 +1,5 @@
 import Ura from "ura";
 
-class Comp {
-    constructor() {
-        this.index = 1; // Initialize index here to avoid undefined error
-        this.vdom = null;
-        this.states = {};
-        this.view = () => <empty></empty>;
-    }
-
-    State(initialValue) {
-        const stateIndex = this.index++;
-        this.states[stateIndex] = initialValue;
-
-        const getter = () => this.states[stateIndex];
-        const setter = (newValue) => {
-            if (!Ura.deepEqual(this.states[stateIndex], newValue)) {
-                this.states[stateIndex] = newValue;
-                this.updateState();
-            }
-        };
-        return [getter, setter];
-    }
-
-    updateState() {
-        console.log("call updateState");
-        const newVDOM = this.view();
-        if (this.vdom) {
-            console.log("old:", this.vdom);
-            console.log("new:", newVDOM);
-            Ura.reconciliate(this.vdom, newVDOM);
-        } else {
-            this.vdom = newVDOM;
-        }
-    }
-
-    render(call) {
-        console.log("render :", call);
-
-        this.view = call;
-        this.vdom = call();
-        return this.vdom;
-    }
-}
-
-
-// Component definitions
 function Child(props) {
     const [render, State] = Ura.init();
     const [getter, setter] = State(20);
@@ -74,7 +29,14 @@ function Tag() {
             >
                 click
             </button>
-            <Child id={getter()} />
+            {/* <if cond={getter() % 2}>
+                <Child id={"a"} />
+
+            </if>
+            <if cond={!getter() % 2}>
+                <Child id={"b"} />
+
+            </if> */}
         </root>
     ));
 }
