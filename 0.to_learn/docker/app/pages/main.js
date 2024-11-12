26256d367787058122a6a4538f961afcc474e3b9
Mohammed Hrima <mhrima@c4r6p1.1337.ma>
2024-11-12 20:03:03 +0100

    adding dockerfile

---
+++ 0.to_learn/docker/app/pages/main.js
@@ -0,0 +1,35 @@
+import Ura from "ura";
+// import dir_routes from "./routes.js";
+// Ura.sync();
+async function loadRoutes() {
+    try {
+        const response = await fetch("/pages/routes.json");
+        const data = await response.json();
+        const { routes, styles, base } = data;
+        console.log(data);
+        
+        if (routes) {
+            for (const route of Object.keys(routes)) {
+                const module = await import(routes[route]);
+                if (!module.default)
+                    throw `${route}: ${routes[route]} must have a default export`;
+                Ura.setRoute(Ura.normalizePath(route), module.default);
+                if (base && route === base)
+                    Ura.setRoute("*", module.default);
+            }
+        }
+        // Load CSS styles
+        styles?.forEach((style) => {
+            Ura.loadCSS("/pages/" + style);
+        });
+        window.addEventListener("hashchange", Ura.refresh);
+        window.addEventListener("DOMContentLoaded", Ura.refresh);
+        window.addEventListener("popstate", Ura.refresh);
+        Ura.refresh();
+        console.log("Routes and styles loaded:", Ura.Routes);
+    }
+    catch (error) {
+        console.error("Error loading JSON or importing modules:", error);
+    }
+}
+loadRoutes();
