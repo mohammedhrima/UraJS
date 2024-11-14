c4bb3c7b475f218eaf658060854ad5870c0bced7
Mohammed Hrima <mhrima@e2r12p1.1337.ma>
2024-11-14 12:16:42 +0100

    update random

--- project/frontend/out/pages/utils/Arrow/Arrow.js
+++
@@ -1,7 +0,0 @@
-import Ura from "ura";
-function Arrow() {
-    const [render, State] = Ura.init();
-    return render(() => (Ura.element("svg", { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24" },
-        Ura.element("path", { d: "M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" }))));
-}
-export default Arrow;
