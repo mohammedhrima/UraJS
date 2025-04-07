e5812c9ac308f0a73568eb4bfb7000afcd4ad0f9
mohammed hrima <71414472+mohammedhrima@users.noreply.github.com>
2025-04-07 01:42:23 +0100

    Update ura.config.js

--- ura.config.js
+++ ura.config.js
@@ -1,14 +1,6 @@
 import { checkConfig, setConfig } from "./scripts/utils.js";
 
 export default (async () => {
-  setConfig({
-    typescript: "disable",
-    dirRouting: "enable",
-    defaultRoute: "home",
-    tailwind: "disable",
-    scss: "disable",
-    css: "enable",
-    port: 17000,
-  })
+  setConfig({})
   await checkConfig();
 })
